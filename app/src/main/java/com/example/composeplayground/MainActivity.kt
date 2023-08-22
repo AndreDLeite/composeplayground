ff281943878a06cf4bcd5450dfa7212f60e64787
andreleite <andreduarteleite@gmail.com>
2023-08-22 20:07:55 -0300

    First commit for compose playground project

---
+++ app/src/main/java/com/example/composeplayground/MainActivity.kt
@@ -0,0 +1,83 @@
+package com.example.composeplayground
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.composeplayground.ui.ColorBox
+import com.example.composeplayground.ui.ImageCard
+import com.example.composeplayground.ui.theme.ComposeplaygroundTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+//            loadImageCard()
+            
+            Column(Modifier.fillMaxSize()) {
+                val color = remember {
+                    mutableStateOf(Color.Yellow)
+                }
+                ColorBox(
+                    Modifier.weight(1f).fillMaxSize()
+                ) {
+                    color.value = it
+                }
+                Box(modifier = Modifier
+                    .background(color.value)
+                    .weight(1f)
+                    .fillMaxSize()
+                )
+            }
+
+        }
+    }
+}
+
+@Composable
+private fun loadImageCard() {
+    val painter = painterResource(id = R.drawable.glados1)
+    val description = "teste"
+    val title = "teste de card em compose"
+    Box(
+        modifier = Modifier
+            .fillMaxWidth(0.5f)
+            .padding(16.dp)
+    ) {
+        ImageCard(
+            painter = painter,
+            contentDescription = description,
+            title = title
+        )
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ComposeplaygroundTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/example/composeplayground/ui/ColorBox.kt
@@ -0,0 +1,28 @@
+package com.example.composeplayground.ui
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import kotlin.random.Random
+
+@Composable
+fun ColorBox(modifier: Modifier = Modifier,
+             updateColor: (Color) -> Unit
+) {
+    Box(modifier = modifier
+        .background(Color.Red)
+        .clickable {
+            updateColor(
+                Color(
+                    Random.nextFloat(),
+                    Random.nextFloat(),
+                    Random.nextFloat(),
+                    1f
+                )
+            )
+        }
+    )
+}
