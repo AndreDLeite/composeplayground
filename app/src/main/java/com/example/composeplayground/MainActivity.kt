458050b6d0bea7029c99db9429f9ca9b65db9e2c
andreleite <andreduarteleite@gmail.com>
2023-08-23 20:11:01 -0300

    Adding lazy column with if statements to change its modifiers based on external values

--- app/src/main/java/com/example/composeplayground/MainActivity.kt
+++ app/src/main/java/com/example/composeplayground/MainActivity.kt
@@ -3,30 +3,36 @@ package com.example.composeplayground
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.composeplayground.ui.ColorBox
-import com.example.composeplayground.ui.ImageCard
+import androidx.compose.ui.unit.sp
 import com.example.composeplayground.ui.theme.ComposeplaygroundTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            LazyColumn {
+                items(500) {
+                    Text(
+                        text = "item $it",
+                        fontSize = if(it % 2 == 0) 16.sp else 8.sp,
+                        fontWeight = FontWeight.Bold,
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(if(it % 2 == 0 ) 12.dp else 20.dp)
+                    )
+                }
+            }
         }
     }
 }
